4d247ea185b0e8cb030b6873fa1a8f362fc7d5e9
Vince Au <vinceau09@gmail.com>
2019-09-30 00:33:00 +1000

    Remove server/socket stuff

--- src/utils/slpWriter.ts
+++ src/utils/slpWriter.ts
@@ -1,63 +1,22 @@
-import net from 'net';
 import _ from 'lodash';
 import { SlpFile, SlpFileMetadata } from './slpFile';
-import { SlpRawStream, SlpRawEvent, Command, PostFrameUpdateType, GameEndType } from 'slp-realtime';
+import { SlpRawStream, SlpRawEvent, Command, PostFrameUpdateType } from 'slp-realtime';
 
 export interface SlpFileWriterOptions {
-  targetFolder: string;
-  id: number;
-  isRelaying: boolean;
   folderPath: string;
   consoleNick: string;
-  onFileStateChange: () => void;
-}
-
-interface ClientData {
-  socket: net.Socket;
-  readPos: number;
-};
-
-export interface HandleDataResponse {
-  isNewGame: boolean;
-  isGameEnd: boolean;
-};
-
-export enum WriteCommand {
-  CMD_RECEIVE_COMMANDS = 0x35,
-  CMD_GAME_START = 0x36,
-  CMD_RECEIVE_POST_FRAME_UPDATE = 0x38,
-  CMD_RECEIVE_GAME_END = 0x39,
 }
 
 export class SlpFileWriter {
-  private server: net.Server;
   private folderPath: string;
-  private onFileStateChange: any;
-  private id: number;
   private consoleNick: string;
   private currentFile: SlpFile | null;
-  private statusOutput: any;
-  private isRelaying: boolean;
-  private clients: Array<ClientData>;
-  private bytesWritten = 0;
   private metadata: SlpFileMetadata;
-  private previousBuffer: Uint8Array = Buffer.from([]);
-  private fullBuffer: Uint8Array = Buffer.from([]);
-  private payloadSizes = new Map<number, number>();
   private rawStream: SlpRawStream;
 
   public constructor(settings: SlpFileWriterOptions) {
     this.folderPath = settings.folderPath;
-    this.onFileStateChange = settings.onFileStateChange;
-    this.id = settings.id;
     this.consoleNick = settings.consoleNick;
-    this.statusOutput = {
-      status: false,
-      timeout: null,
-    };
-    this.isRelaying = settings.isRelaying;
-    this.clients = [];
-    this.manageRelay();
     this.metadata = {
       lastFrame: -124,
       players: {},
@@ -75,102 +34,19 @@ export class SlpFileWriter {
       console.log("new game started");
       this._handleNewGame();
     })
-    this.rawStream.on(SlpRawEvent.GAME_END, (command: Command, payload: GameEndType) => {
+    this.rawStream.on(SlpRawEvent.GAME_END, () => {
       console.log("game ended");
-      this._handleEndGame(command, payload);
+      this._handleEndGame();
     })
   }
 
-  public manageRelay(): void {
-    if (!this.isRelaying) {
-      if (this.server) {
-        this.server.close();
-      }
-
-      this.server = null;
-      this.clients = [];
-
-      return;
-    }
-    
-    if (this.server) {
-      // If server is already up, no need to start
-      return;
-    }
-    
-    this.server = net.createServer((socket) => {
-      socket.setNoDelay().setTimeout(20000);
-
-      const clientData: ClientData = {
-        socket: socket,
-        readPos: 0,
-      };
-
-      this.clients.push(clientData);
-      socket.on("close", (err) => {
-        if (err) console.log(err);
-        _.remove(this.clients, (client) => socket === client.socket);
-      });
-    });
-    this.server.listen(666 + this.id, '0.0.0.0');
-  }
-
-  public getCurrentFilePath(): string {
-    return this.currentFile.path();
-  }
-
   public updateSettings(settings: SlpFileWriterOptions): void {
-    this.folderPath = settings.targetFolder;
-    this.id = settings.id;
-    this.isRelaying = settings.isRelaying;
+    this.folderPath = settings.folderPath;
     this.consoleNick = settings.consoleNick || this.consoleNick;
-    this.manageRelay();
-  }
-
-  public handleStatusOutput(timeoutLength = 100): void {
-    const setTimer = (): void => {
-      if (this.statusOutput.timeout) {
-        // If we have a timeout, clear it
-        clearTimeout(this.statusOutput.timeout);
-      }
-
-      this.statusOutput.timeout = setTimeout(() => {
-        // If we timeout, set and set status
-      }, timeoutLength);
-    }
-
-    if (this.metadata.lastFrame < -60) {
-      // Only show the source in the later portion of the game loading stage
-      return;
-    }
-
-    if (this.statusOutput.status) {
-      // If game is currently active, reset the timer
-      setTimer();
-      return;
-    }
-
-    // Set timer
-    setTimer();
   }
 
   public handleData(newData: Uint8Array): void {
     this.rawStream.write(newData);
-
-    // Write data to relay, we do this after processing in the case there is a new game, we need
-    // to have the buffer ready
-    this.fullBuffer = Buffer.concat([this.fullBuffer, newData]);
-
-    if (this.clients) {
-      const buf = this.fullBuffer;
-      _.each(this.clients, (client) => {
-        client.socket.write(buf.slice(client.readPos));
-
-        // eslint doesn't like the following line... I feel like it's a valid use case but idk,
-        // maybe there's risks with doing this?
-        client.readPos = buf.byteLength; // eslint-disable-line
-      });
-    }
   }
 
   private _handleNewGame(): void {
@@ -178,12 +54,6 @@ export class SlpFileWriter {
       folderPath: "./",
       consoleNick: "hello",
     });
-
-    // Clear clients back to position zero
-    this.clients = _.map(this.clients, client => ({
-      ...client,
-      readPos: 0,
-    }));
   }
 
   private _handlePostFrameUpdate(command: number, payload: PostFrameUpdateType): void {
@@ -215,20 +85,13 @@ export class SlpFileWriter {
     this.metadata.players[`${playerIndex}`] = player;
   }
 
-  private _handleEndGame(command: Command, payload: GameEndType): void {
-    const endMethod = payload.gameEndMethod;
-    if (endMethod !== 7) {
-      this.handleStatusOutput(700);
-    }
-
+  private _handleEndGame(): void {
     // End the stream
     this.currentFile.setMetadata(this.metadata);
     this.currentFile.end();
     console.log("Finished writing file.");
     // Clear current file
     this.currentFile = null;
-    // Update file state
-    this.onFileStateChange();
   }
 
 }
