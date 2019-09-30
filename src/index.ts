43a8a26d942a2ed7ddc0f6472d406b4201c92809
Vince Au <vinceau09@gmail.com>
2019-09-30 19:57:02 +1000

    index.ts: actually export things

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,2 @@
-import { ConsoleConnection, ConnectionEvent, ConnectionDetails } from "./console/connection";
-import { SlpFileWriter } from "./utils/slpWriter";
-
-const slpWriter = new SlpFileWriter({
-  folderPath: "./",
-  consoleNick: "unknown"
-});
-
-const conn = new ConsoleConnection({
-  ipAddress: "0.0.0.0",
-  port: 1667,
-});
-
-conn.on(ConnectionEvent.DATA, (data: Buffer) => {
-  slpWriter.handleData(data);
-})
-
-conn.on(ConnectionEvent.HANDSHAKE, (settings: ConnectionDetails) => {
-  slpWriter.updateSettings({
-    consoleNick: settings.consoleNick,
-  });
-})
-
-conn.connect();
+export { ConsoleConnection, ConnectionEvent, ConnectionDetails } from "./console/connection";
+export { SlpFileWriter } from "./utils/slpWriter";
